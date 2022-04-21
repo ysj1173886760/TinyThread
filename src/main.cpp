eb9aec33409188a578a2984a2f9d2bcb5b7fb1dc
ysj1173886760 <1173886760@qq.com>
2022-04-21 15:48:02 +0800

    fix implementation of quicksort, previously i was using merge sort

--- src/main.cpp
+++ src/main.cpp
@@ -10,6 +10,19 @@
 int max_size = 1000000;
 int threshold = 100;
 
+int partition(int *array, int l, int r) {
+    int x = array[r - 1];
+    int cur = l - 1;
+    for (int i = l; i < r - 1; i++) {
+        if (array[i] < x) {
+            cur++;
+            std::swap(array[i], array[cur]);
+        }
+    }
+    std::swap(array[cur + 1], array[r - 1]);
+    return cur + 1;
+}
+
 void quick_sort(void *args) {
     Arg *arg = (Arg*)args;
     LOG_INFO("%p: %d %d", std::this_thread::get_id(), arg->left, arg->right);
@@ -17,7 +30,8 @@ void quick_sort(void *args) {
     if (arg->right - arg->left < threshold) {
         std::sort(arg->array + arg->left, arg->array + arg->right);
     } else {
-        int middle = (arg->right + arg->left) / 2;
+        // int middle = (arg->right + arg->left) / 2;
+        int middle = partition(arg->array, arg->left, arg->right);
         
         // now i see why we need gc
         Arg *left_arg = new Arg;
@@ -25,7 +39,7 @@ void quick_sort(void *args) {
         WaitGroup *child_wg = new WaitGroup(1);
         left_arg->left = arg->left;
         left_arg->right = middle;
-        right_arg->left = middle;
+        right_arg->left = middle + 1;
         right_arg->right = arg->right;
         left_arg->array = arg->array;
         right_arg->array = arg->array;
@@ -36,35 +50,39 @@ void quick_sort(void *args) {
         // quick_sort(left_arg);
 
         LOG_INFO("%p: %d %d", std::this_thread::get_id(), arg->left, arg->right);
-        check_stack();
+        // check_stack();
 
         quick_sort(right_arg);
+        // create(quick_sort, right_arg);
+
+        // a pitfall here, WaitGroup will busy waiting
+        // maybe switch to another task would be greater?
         child_wg->wait();
         
         // do the 2-way merge
-        int *tmp = new int[arg->right - arg->left];
-        int left = arg->left, right = middle;
-        int cnt = 0;
-        while (left < middle && right < arg->right) {
-            if (arg->array[left] < arg->array[right]) {
-                tmp[cnt++] = arg->array[left++];
-            } else {
-                tmp[cnt++] = arg->array[right++];
-            }
-        }
-        while (left < middle) {
-            tmp[cnt++] = arg->array[left++];
-        }
-        while (right < arg->right) {
-            tmp[cnt++] = arg->array[right++];
-        }
-        // copy back
-        for (int i = 0; i < cnt; i++) {
-            arg->array[i + arg->left] = tmp[i];
-        }
-        assert(cnt == arg->right - arg->left);
+        // int *tmp = new int[arg->right - arg->left];
+        // int left = arg->left, right = middle;
+        // int cnt = 0;
+        // while (left < middle && right < arg->right) {
+        //     if (arg->array[left] < arg->array[right]) {
+        //         tmp[cnt++] = arg->array[left++];
+        //     } else {
+        //         tmp[cnt++] = arg->array[right++];
+        //     }
+        // }
+        // while (left < middle) {
+        //     tmp[cnt++] = arg->array[left++];
+        // }
+        // while (right < arg->right) {
+        //     tmp[cnt++] = arg->array[right++];
+        // }
+        // // copy back
+        // for (int i = 0; i < cnt; i++) {
+        //     arg->array[i + arg->left] = tmp[i];
+        // }
+        // assert(cnt == arg->right - arg->left);
 
-        delete[] tmp;
+        // delete[] tmp;
     }
     if (arg->wg) {
         arg->wg->done();
